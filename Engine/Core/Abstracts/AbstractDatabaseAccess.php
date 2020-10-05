78f9bbab17ae7f79c293db360e2ec67351105556
FK <fk@7pkonzepte.de>
2020-10-05 18:06:48 +0200

    Change visibility of AbstractDatabaseAccess methods

--- Engine/Core/Abstracts/AbstractDatabaseAccess.php
+++ Engine/Core/Abstracts/AbstractDatabaseAccess.php
@@ -2,9 +2,7 @@
 
 namespace Oforge\Engine\Core\Abstracts;
 
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\ORMException;
 use Oforge\Engine\Core\Forge\ForgeEntityManager;
 
 /**
@@ -31,25 +29,12 @@ abstract class AbstractDatabaseAccess {
     }
 
     /** @return ForgeEntityManager */
-    public function entityManager() : ForgeEntityManager {
+    protected function entityManager() : ForgeEntityManager {
         if (!isset($this->forgeEntityManger)) {
             $this->forgeEntityManger = Oforge()->DB()->getForgeEntityManager();
         }
 
         return $this->forgeEntityManger;
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return EntityRepository
-     */
-    public function repository(string $name = 'default') : EntityRepository {
-        if (!isset($this->repositories[$name])) {
-            $this->repositories[$name] = $this->entityManager()->getRepository($this->models[$name]);
-        }
-
-        return $this->repositories[$name];
     }
 
     /**
@@ -59,8 +44,21 @@ abstract class AbstractDatabaseAccess {
      *
      * @return EntityRepository
      */
-    public function getRepository(string $class) : EntityRepository {
+    protected function getRepository(string $class) : EntityRepository {
         return $this->entityManager()->getRepository($class);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return EntityRepository
+     */
+    protected function repository(string $name = 'default') : EntityRepository {
+        if (!isset($this->repositories[$name])) {
+            $this->repositories[$name] = $this->entityManager()->getRepository($this->models[$name]);
+        }
+
+        return $this->repositories[$name];
     }
 
 }
