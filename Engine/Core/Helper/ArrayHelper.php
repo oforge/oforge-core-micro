974da7e8f9c5f38c7b7ee372e64f0fd3ea57bef0
FK <fk@7pkonzepte.de>
2020-10-06 14:45:18 +0200

    Add ArrayHelper::dotExist method

--- Engine/Core/Helper/ArrayHelper.php
+++ Engine/Core/Helper/ArrayHelper.php
@@ -103,6 +103,7 @@ class ArrayHelper {
 
     /**
      * Extract all data by key with original value.
+     *
      * @param array $inputArray
      * @param array $keys
      *
@@ -143,6 +144,18 @@ class ArrayHelper {
     }
 
     /**
+     * Check if value by key in dot notation exist.
+     *
+     * @param array $array
+     * @param string $key
+     *
+     * @return bool
+     */
+    public static function dotExist(array $array, string $key) : bool {
+        return self::dotGet($array, $key, null) === self::dotGet($array, $key, false);
+    }
+
+    /**
      * Get value by key in dot notation or $default if not exist.
      *
      * @param array $array
@@ -152,7 +165,7 @@ class ArrayHelper {
      * @return mixed
      */
     public static function dotGet(array $array, string $key, $default = null) {
-        if (empty($key) || empty($array)) {
+        if (empty($array) || empty($key)) {
             return $default;
         }
         if (strpos($key, '.') !== false) {
@@ -186,7 +199,7 @@ class ArrayHelper {
             $keyOrKeyPath = explode('.', $keyOrKeyPath);
         }
         if (is_array($keyOrKeyPath)) {
-            $tmp  = &$array;
+            $tmp = &$array;
             foreach ($keyOrKeyPath as $key) {
                 if (!isset($tmp[$key])) {
                     $tmp[$key] = [];
